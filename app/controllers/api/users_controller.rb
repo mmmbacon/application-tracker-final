a38d69325e3b59c8309d76f77160a37823bc64e9
Brandon Macdonald <bmacdonald1986@gmail.com>
2021-06-05 17:56:34 -0600

    Delete redundant data in user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -16,18 +16,4 @@ class Api::UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
-
-  def create
-  end
-
-  def destroy
-  end
-
-  def update
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  end
 end
