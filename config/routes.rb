5eb8543120b62b04d72b53c9151c3970955be08c
Courtney Amos <c.amos@shaw.ca>
2021-06-06 11:32:26 -0700

    setup the sessions controller and matching routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   #get 'static/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
   namespace :api, defaults: { format: 'json' } do
     resources :users, only: [:create, :show, :index]
     resources :jobs, only: [:index, :create, :destroy, :update]
